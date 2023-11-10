f64eb03dea68dc365a7ae86bd7178cb3305beae5
harsha-muppalla <143773780+harsha-muppalla@users.noreply.github.com>
2023-11-10 17:36:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import { Home } from './components/Portfolio';
-function App() {
-  return (
-    <div className="App">
-      <Home></Home>
+import React from 'react';
+import Home from './components/portfolio';
 
+const App = () => {
+  return (
+    <div>
+      <Home />
     </div>
   );
-}
+};
 
 export default App;
